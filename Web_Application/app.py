47ec570236d025657843e6486db0befac059a3e1
Itgel Ganbold <itgel98@gmail.com>
2023-04-16 14:24:45 +0100

    port update

--- Web_Application/app.py
+++ Web_Application/app.py
@@ -241,4 +241,4 @@ def render():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, host = '0.0.0.0', port=80)
+    app.run(debug=True, host = '0.0.0.0', port=5000)
